428ab98b6833ff81179de9c62bdb5ecc762f6371
Bombastion <tyler.tift@gmail.com>
2025-05-23 07:36:24 -0600

    Adding post for containers, input validation

--- models/containers.ts
+++ models/containers.ts
@@ -1,7 +1,9 @@
 // Represents the type of container an item might be in
 export class ItemContainer {
     id: number;
+    // Represents the type of container, e.g., "Craftmaster" or "Crowler"
     containerName: string;
+    // How we would like the container to show up in the system, e.g., "Full Pour" or "Taster"
     displayName: string;
     price: number;
 
--- routes/containers.ts
+++ routes/containers.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { DataProvider } from "../storage/providers";
+import { ItemContainer } from '../models/containers';
 
 export class ContainersRoutes {
   router = express.Router();
@@ -10,11 +11,48 @@ export class ContainersRoutes {
     this.registerRoutes();
   }
 
+  validateInput(req: Request, res: Response, requiredFieldsAndTypes: Record<string, string>): boolean {
+    /*
+      Generically validates inputs.
+      Specifically, makes sure that all the keys of requiredFieldsAndTypes are provided, and that the values on req.body
+      are the correct types based on the corresponding values in the Record object
+
+      Returns: false if an error was found, true otherwise
+    */
+    let missingFields: Array<string> = [];
+    let badTypes: Array<string> = [];
+    Object.keys(requiredFieldsAndTypes).forEach(function(fieldName) {
+      if (!req.body[fieldName]) {
+        missingFields.push(fieldName);
+      } else {
+        if (requiredFieldsAndTypes[fieldName] === "string" && typeof(req.body[fieldName]) !== "string") {
+          badTypes.push(`${fieldName} must be of type string`);
+        }
+        if (requiredFieldsAndTypes[fieldName] === "number" && typeof(req.body[fieldName]) !== "number") {
+          badTypes.push(`${fieldName} must be of type number`);
+        }
+      }
+    });
+    if (missingFields.length > 0) {
+      res.status(422).send(`All of [${missingFields.join(", ")}] must be provided`);
+      return false;
+    }
+    if (badTypes.length > 0) {
+      res.status(400).send(`The following type errors were found: [${badTypes.join(", ")}]`);
+      return false;
+    }
+
+    return true;
+  }
+
   registerRoutes(): void {
+    // Default get
+    // TODO: Should probably list all IDs or something eventually
     this.router.get("/", (_req: Request, res: Response) => {
       res.send("Look at all the glasses!");
     });
 
+    // Gets a specific container by ID
     this.router.get("/:containerId", (req: Request, res: Response) => {
       const result = this.dataProvider.getContainer(parseInt(req.params.containerId));
       if (result !== null) {
@@ -23,6 +61,19 @@ export class ContainersRoutes {
       }
       res.sendStatus(404);
       return
+    });
+
+    // Creates a new container with the given info
+    this.router.post("/", (req: Request, res: Response) => {
+      const requiredFieldsAndTypes: Record<string, string> = {"containerName": "string", "displayName": "string", "price": "number"};
+      if(!this.validateInput(req, res, requiredFieldsAndTypes)) {
+        return;
+      }
+
+      let container = new ItemContainer(0, req.body.containerName, req.body.displayName, req.body.price);
+      container = this.dataProvider.addContainer(container);
+
+      res.send(container);
     });
   }
 }
