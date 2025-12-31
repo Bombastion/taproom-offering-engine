492f211b7ae6849b61da6ddca6f083e21668fbdd
Bombastion <tyler.tift@gmail.com>
2025-12-30 21:53:14 -0700

    Dealing with stupid async stuff. Glad I pushed through that XD

--- routes/breweries.ts
+++ routes/breweries.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import Routes from './common';
-import { Item } from '../models/items';
 
+const sharp = require('sharp');
 
 export class BreweriesRoutes extends Routes {
   // Order matters here. If we don't specify non-IDs first, they get interpreted as IDs
@@ -10,6 +10,12 @@ export class BreweriesRoutes extends Routes {
       const breweryList = this.dataProvider.getBreweries();
       res.render("breweryList", {breweries: breweryList});
       return 
+    });
+
+    this.router.post("", (req: Request, res: Response) => {
+      console.log(req.body);
+      res.redirect("/breweries/manage");
+      return
     });
 
     this.router.get("/:breweryId", (req: Request, res: Response) => {
