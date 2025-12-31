bebb6106872d1e545740e2e80498d4e2d574c76d
Bombastion <tyler.tift@gmail.com>
2025-12-30 23:14:35 -0700

    Build out add behavior in backend

--- routes/breweries.ts
+++ routes/breweries.ts
@@ -14,9 +14,32 @@ export class BreweriesRoutes extends Routes {
     });
 
     this.router.post("", (req: Request, res: Response) => {
-      this.dataProvider.addBrewery(new Brewery(2, req.body['newBreweryName'], req.body['newBreweryLogoB64'], req.body['newBreweryLocation']))
+      this.dataProvider.addBrewery(new Brewery(2, req.body['newBreweryName'], req.body['newBreweryLogoB64'], req.body['newBreweryLocation']));
       
       res.redirect(req.query.callbackUrl as string);
+      return
+    });
+
+    this.router.patch("/:breweryId", (req: Request, res: Response) => {
+      try{
+        if (!req.body) {
+          res.status(400).send("Request body expected");
+          return
+        }
+        const result = this.dataProvider.updateBrewery(parseInt(req.params.breweryId), new Brewery(null, req.body['newBreweryName'], req.body['newBreweryLogoB64'], req.body['newBreweryLocation']));
+        if (result !== null) {
+          res.send(result);
+          return
+        }
+        res.status(400);
+        res.send("Invalid Argument");
+      } catch(e: any) {
+        const statusCode = "statusCode" in e ? e["statusCode"] : 500;
+        const message = "message" in e ? e["message"] : "Unexpected error occurred";
+        res.status(statusCode);
+        res.send(message);
+      }
+      
       return
     });
 
